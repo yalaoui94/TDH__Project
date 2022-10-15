3408b5c89d7f4a3284b7cc209f2ade9ee5e5adc9
Yalaoui <yalaouihicham13@gmail.com>
2022-10-15 22:58:04 +0200

    modification du footer

--- src/Components/footer/Footer.jsx
+++ src/Components/footer/Footer.jsx
@@ -11,10 +11,10 @@ const Footer = () => {
       <ul className='permalinks'>
         <li><a href="#">Home</a></li>
         <li><a href="#about">About</a></li>
-        <li><a href="#experiece">Experience</a></li>
-        <li><a href="#portfolio">Portfolio</a></li>
-        <li><a href="#testimonials">Testimonials</a></li>
-        <li><a href="#contact">Contact</a></li>
+        <li><a href="#Experiece">Experience</a></li>
+        <li><a href="#Portfolio">Portfolio</a></li>
+        <li><a href="#Testimonials">Testimonials</a></li>
+        <li><a href="#Contact">Contact</a></li>
       </ul>
       <div className='footer__socials'>
         <a href="https://facebook.com"><FaFacebook/></a>
